5a092557543274e9ee8622838890a9abf1710926
javier27490 <84250045+javier27490@users.noreply.github.com>
2022-05-02 14:15:43 -0300

    whatsapp js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const evento = document.getElementById('enviar')
+const enviarFormulario =() => {
+        let nombre = document.getElementById('correo').value;
+        let apellido = document.getElementById('asunto').value;
+        let mensaje = document.getElementById('texto').value;
+        let numero= 03426142087;
+        var win= window.open(`https://wa.me/${numero}?text=Hola%20mi%20nombre%20es%20${correo}
+        %20${asunto}, Asunto:%20${texto}`,'_blank');       
+}
+evento.addEventListener('click', enviarFormulario)
